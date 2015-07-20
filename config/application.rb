794fdb5c08716dab7d4497316d654acb48357ac6
Bryant Novas <bryant.novas@gmail.com>
2015-07-20 07:24:26 -0400

    assets precompiled

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module DollarPizzaApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.serve_static_assets = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
     config.active_record.raise_in_transactional_callbacks = true
